71de03ae3abed8d059e1df1aadde5f03d0bb9714
Pedro Boado <pboado@apache.org>
2018-06-18 22:01:21 +0000

    Update download page - included apache moderator changes

--- site/publish/js/download.js
+++ site/publish/js/download.js
@@ -1,28 +1,30 @@
-var apacheUrl = "http://www.apache.org/dist/phoenix/"
+var apacheUrlHttp = "http://www.apache.org/dist/phoenix/"
+var apacheUrlHttps = "https://www.apache.org/dist/phoenix/"
 var dynUrl = "http://www.apache.org/dyn/closer.lua/phoenix/"
 
 function parcelFolderHtml(version) {
-    return '<li><a href="' + apacheUrl + 'apache-phoenix-' + version + '/parcels/">parcels</a></li>';
+    return '<li><a href="' + apacheUrlHttp + 'apache-phoenix-' + version + '/parcels/">parcels</a></li>';
 }
 
-function addRelease(version, date, commit) {
+function addRelease(version, date) {
     var tr = document.createElement('tr');
     var parcelsHtml = version.includes('-cdh') ? parcelFolderHtml(version) : ''
     tr.innerHTML =
         '<td>' + version + '</td>' +
         '<td>' + date + '</td>' +
-        '<td><a href="https://git-wip-us.apache.org/repos/asf?p=phoenix.git;a=commit;h=' + commit + '">' + commit.substring(0,12) + '</a></td>' +
         '<td><ul><li>' +
-          '<a href="' + dynUrl + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz">bin</a> ( ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.sha256">sha256</a> ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.sha512">sha512</a> ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.asc">asc</a> ' +
-        ')</li><li>' +
-          '<a href="' + dynUrl + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz">src</a> ( ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.sha256">sha256</a> ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.sha512">sha512</a> ' +
-          '<a href="' + apacheUrl + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.asc">asc</a> ' +
-        ')</li>' +  parcelsHtml +
+          '<a href="' + dynUrl + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz">bin</a> ' +
+          // '<a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.sha256">sha256</a> ' +
+          '&nbsp;&nbsp;' +
+          '[ <a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.sha512">sha512</a>' +
+          ' | <a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/bin/apache-phoenix-' + version + '-bin.tar.gz.asc">asc</a> ]' +
+        '</li><li>' +
+          '<a href="' + dynUrl + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz">src</a> ' +
+          // '<a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.sha256">sha256</a> ' +
+          '&nbsp;&nbsp;' +
+          '[ <a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.sha512">sha512</a>' +
+          ' | <a href="' + apacheUrlHttps + 'apache-phoenix-' + version + '/src/apache-phoenix-' + version + '-src.tar.gz.asc">asc</a> ]' +
+        '</li>' +  parcelsHtml +
         '</ul></td>';
     document.getElementById('releases').appendChild(tr);
 }
--- site/source/src/site/markdown/download.md
+++ site/source/src/site/markdown/download.md
@@ -6,12 +6,13 @@ See our installation instructions [here](installation.html), our release notes [
 and a list of fixes and new features [here](https://issues.apache.org/jira/secure/ReleaseNote.jspa?version=12334393&projectId=12315120).
 Follow [Verify the Integrity of the Files](https://www.apache.org/dyn/closer.cgi#verify) for how to verify your mirrored downloads.
 
+Current release 4.14 can run on Apache HBase 0.98, 1.1, 1.2, 1.3 and 1.4 and CDH HBase 5.11, 5.12, 5.13 and 5.14. Apache HBase 2.0 is supported by 5.0.0-alpha. Please follow the appropriate link depending on your HBase version. 
+ 
 
 <table id="releases">
 <tr>
-    <th>Version</th>
-    <th>Release Date</th>
-    <th>Commit</th>
+    <th width='140'>Version</th>
+    <th width='100'>Release Date</th>
     <th>Download</th>
 </tr>
 </table>
@@ -21,15 +22,15 @@ If you are looking for an old release that is not present here or on the mirror,
 <script src="/js/download.js"></script> 
 <script>
 //<![CDATA[
-addRelease('4.14.0-HBase-0.98','09/jun/2018','22b3b206c656aed088f3f25cfef9cae669923ea7');
-addRelease('4.14.0-HBase-1.1','09/jun/2018','6a365b8713542486b2ae46032d59c9b22d3efe43');
-addRelease('4.14.0-HBase-1.2','09/jun/2018','9db22d8b7658d56b14650d90996cbf50747dd4d9');
-addRelease('4.14.0-HBase-1.3','09/jun/2018','c27c68cf1e183d450cf21c20f3994e3192479e46');
-addRelease('4.14.0-HBase-1.4','09/jun/2018','a7adbbcca749cd69de808195a09328c330e731c5');
-addRelease('4.14.0-cdh5.11.2','09/jun/2018','0a06e5964f3f7634cd8d70208fc1b07e2890cb1f');
-addRelease('4.14.0-cdh5.12.2','09/jun/2018','44939d29e04d07cc668c93437c805f8dcf60cd58');
-addRelease('4.14.0-cdh5.13.2','09/jun/2018','b68b9e34c6ece256c6b57edfbe0431af1fc27eb6');
-addRelease('4.14.0-cdh5.14.2','09/jun/2018','3c72eef250ffd6ea172d273121a400a944d02843');
-addRelease('5.0.0-alpha-HBase-2.0','09/feb/2018','451d6a37d0d461b60edff36ceb42b17bb9610350');
+addRelease('4.14.0-HBase-1.4','09/jun/2018');
+addRelease('4.14.0-HBase-1.3','09/jun/2018');
+addRelease('4.14.0-HBase-1.2','09/jun/2018');
+addRelease('4.14.0-HBase-1.1','09/jun/2018');
+addRelease('4.14.0-HBase-0.98','09/jun/2018');
+addRelease('4.14.0-cdh5.14.2','09/jun/2018');
+addRelease('4.14.0-cdh5.13.2','09/jun/2018');
+addRelease('4.14.0-cdh5.12.2','09/jun/2018');
+addRelease('4.14.0-cdh5.11.2','09/jun/2018');
+addRelease('5.0.0-alpha-HBase-2.0','09/feb/2018');
 //]]>
 </script>
