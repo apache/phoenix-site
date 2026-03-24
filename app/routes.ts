6f250e8573be307abc5efcc7a2099a46791e2aa2
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2026-03-24 11:25:10 +0100

    Update 404

--- app/routes.ts
+++ app/routes.ts
@@ -38,8 +38,7 @@ export default [
     route("issues-tracking", "routes/_landing/issues-tracking.tsx"),
     route("source-repository", "routes/_landing/source-repository.tsx"),
     route("tech-talks", "routes/_landing/tech-talks.tsx"),
-    route("presentations", "routes/_landing/presentations.tsx"),
-    route("404", "routes/404.tsx")
+    route("presentations", "routes/_landing/presentations.tsx")
   ]),
   // Docs
   layout("./pages/_docs/docs-layout.tsx", [
