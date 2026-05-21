ae8e6ee11785f840e0b92af3cbaa2735f59a3cf8
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2026-05-21 16:31:07 +0200

    PHOENIX-7807 Addendum: add dark mode support for 404 page (#22)

--- unit-tests/generate-sitemap.test.ts
+++ unit-tests/generate-sitemap.test.ts
@@ -81,9 +81,6 @@ describe("isRedirectOnlyPage", () => {
 describe("shouldIncludeInSitemap", () => {
   it("excludes explicitly ignored html paths", () => {
     expect(shouldIncludeInSitemap("404.html", "<html></html>")).toBe(false);
-    expect(shouldIncludeInSitemap("404/index.html", "<html></html>")).toBe(
-      false
-    );
     expect(
       shouldIncludeInSitemap("phoenixcon-2018/index.html", "<html></html>")
     ).toBe(false);
