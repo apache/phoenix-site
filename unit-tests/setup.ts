2a862f2190682ac3e890e536c577ab3791326f59
Andrew Purtell <apurtell@apache.org>
2026-08-04 17:19:27 -0700

    PHOENIX-7981 Site build improvements (#38)

--- unit-tests/setup.ts
+++ unit-tests/setup.ts
@@ -20,6 +20,30 @@ import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+if (!globalThis.localStorage) {
+  const values = new Map<string, string>();
+  const localStorage: Storage = {
+    get length() {
+      return values.size;
+    },
+    clear: () => {
+      values.clear();
+    },
+    getItem: (key) => values.get(key) ?? null,
+    key: (index) => [...values.keys()][index] ?? null,
+    removeItem: (key) => {
+      values.delete(key);
+    },
+    setItem: (key, value) => {
+      values.set(key, String(value));
+    }
+
+  Object.defineProperty(globalThis, "localStorage", {
+    configurable: true,
+    value: localStorage
+  });
+}
+
 afterEach(() => {
   cleanup();
 });
